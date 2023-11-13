d515785ff74c62b42093df60125bcbe7f1dd54d7
marijast28 <s.marija2808@gmail.com>
2023-11-13 16:06:05 -0500

    OddOrEven MC

---
+++ src/OddOrEven_MC.java
@@ -0,0 +1,22 @@
+public class OddOrEven_MC {
+
+public static void OddOrEven (int num){
+
+    if(num%2 == 0){
+        System.out.println(num + " even");
+    }else{
+        System.out.println(num+ " odd");
+    }
+
+
+}
+
+
+    public static void main(String[] args) {
+
+
+    OddOrEven(67);
+
+    }
+
+}
