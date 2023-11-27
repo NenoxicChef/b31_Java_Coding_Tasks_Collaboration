74ff61a87b951fa719f25ae54b384e89f0532507
gokhanalay <140934358+gokhanalay@users.noreply.github.com>
2023-11-27 12:16:44 -0800

    changes added

--- src/week3/PrimeNumbers_GG.java
+++ src/week3/PrimeNumbers_GG.java
@@ -2,7 +2,7 @@ package week3;
 
 public class PrimeNumbers_GG {
     public static void main(String[] args) {
-        System.out.println(isPrime(7));
+        System.out.println(isPrime(7)); // changes
     }
     public static boolean isPrime (int num){
 
