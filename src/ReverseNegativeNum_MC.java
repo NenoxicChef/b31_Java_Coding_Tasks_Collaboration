afd4b400c82576b3b273c1a3d20ee3b0f57d8b45
marijast28 <s.marija2808@gmail.com>
2023-11-28 14:11:16 -0500

    created Reverse Negative num method

---
+++ src/ReverseNegativeNum_MC.java
@@ -0,0 +1,23 @@
+public class ReverseNegativeNum_MC {
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseNegative(23));
+
+
+    }
+
+
+    public static int reverseNegative(int num){
+
+        if(num < 0){
+            return -num;
+        }else{
+            return num;
+        }
+
+    }
+}
+/*
+write a return method that can reverse negative number and return it as int
+ */
