79422175c8dd2e4f2aba9fd39236e048bb83c1af
ThomasCrown2023 <pickme2day@gmail.com>
2023-12-07 03:59:47 -0800

    Made a change to Unique Char

--- src/week5/UniqueChar_DJ.java
+++ src/week5/UniqueChar_DJ.java
@@ -25,12 +25,7 @@ public class UniqueChar_DJ {
                 return result;
             }
         }
-        /*
-                                    String -- Find the unique
-                                    Write a return method that can find the unique characters from the String
-                                    Ex: unique("AAABBBCCCDEF") ==> "DEF";
 
-         */
 
 
 
