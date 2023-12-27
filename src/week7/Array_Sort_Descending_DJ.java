07c8def9dfc8bf8d29c5bf81fd69d106f219f9f3
ThomasCrown2023 <pickme2day@gmail.com>
2023-12-26 23:14:31 -0800

    Created Array sort descending

---
+++ src/week7/Array_Sort_Descending_DJ.java
@@ -0,0 +1,33 @@
+package week7;
+
+import java.util.Arrays;
+
+public class Array_Sort_Descending_DJ {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(sortDescending(12, 2, 6, 3, 94, -2, 11, 45)));
+        //This will print the sorted numbers in descending order by calling the sortDescending method that I created
+    }
+
+    public static int[] sortDescending(int... arr) { // right here I declared a static method that will take integer arguments and return
+        //an array of integers that are the sorted result
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j < arr.length -1; j++) { // Iterating through the array multiple times comparing the elements
+                //and swapping them to put them in the correct order
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+
+        return arr;
+    }
+
+}
