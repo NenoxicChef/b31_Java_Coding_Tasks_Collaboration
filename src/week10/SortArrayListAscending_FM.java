c4d070b6e0afc9e79704d7d2c8d1b6ce49cc177e
fakhria marouf <fakhriamarouf96@gmail.com>
2024-02-06 11:54:47 -0800

    Week10 Tasks Solved

---
+++ src/week10/SortArrayListAscending_FM.java
@@ -0,0 +1,42 @@
+package week10;
+
+import java.util.ArrayList;
+
+public class SortArrayListAscending_FM {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(2);
+        numbers.add(8);
+        numbers.add(1);
+        numbers.add(3);
+
+
+        sortArrayList(numbers);
+
+
+        System.out.println("Sorted ArrayList: " + numbers);
+    }
+
+    public static void sortArrayList(ArrayList<Integer> arr) {
+        int n = arr.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr.get(j) > arr.get(j + 1)) {
+
+                    int temp = arr.get(j);
+                    arr.set(j, arr.get(j + 1));
+                    arr.set(j + 1, temp);
+                }
+            }
+        }
+    }
+}
+/*
+ArrayList - sorting in ascending
+Write a method that can sort the ArrayList in Ascending order without using
+the sort method.
+ */
